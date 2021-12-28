d2f167b48d5594846cdb97274b94753c70cbe1de
George Weingart <george.weingart@gmail.com>
2021-12-27 20:34:55 -0800

    Added statements to Dockerfile for panphlan and added panphlan

--- panphlan/panphlan_wrapper.sh
+++ panphlan/panphlan_wrapper.sh
@@ -4,10 +4,10 @@ Pangenome=$2
 output1=$3
 Outlocation=$(mktemp -d)
 mkdir $Outlocation/map_results
-TD="/galaxy-central/tools/panphlan"
-python ${TD}/panphlan/panphlan_map.py -i ${Infile} \
+TD="/galaxy-central/tools/panphlan/panphlan"
+python ${TD}/panphlan_map.py -i ${Infile} \
                          --indexes ${TD}/${Pangenome}/${Pangenome} \
                          -p ${TD}/${Pangenome}/${Pangenome}_pangenome.tsv \
                          -o ${Outlocation}/map_results/Results.tsv
 cp ${Outlocation}/map_results/Results.tsv $output1
-
+rm -r $Outlocation
