b3820385800e74457b52bf4681482a30822d62a6
root <>
2022-02-20 17:05:32 +0000

    Updated lm_wrpper

--- mmuphin/lm_meta_wrapper.R
+++ mmuphin/lm_meta_wrapper.R
@@ -8,8 +8,6 @@ option_list = list(
               help="feature_abd"),
     make_option(c("-b", "--batch"), action="store", default="studyID", type='character',
               help="Batch"),
-    make_option(c("-x", "--xcovariates"), action="store", default=c("gender", "age", "BMI"), type='character',
-              help="xcovariates"),
     make_option(c("-c", "--covariates"), action="store", default="gender, age, BMI", type='character',
               help="covariates"),
     make_option(c("-d", "--data"), action="store", default="CRC_meta.Rda", type='character',
@@ -24,29 +22,15 @@ load(opt$data)
 cov0 <- gsub(' ','',opt$covariates)
 cov1 <- strsplit(cov0, split = ",") 
 cov2 <- as.vector(cov1)
-xx <-vector()
-xx<-append(cov2[[1]][1],xx)
-xx<-append(cov2[[1]][2],xx,after = 1)
-xx<-append(cov2[[1]][3],xx, after = 2)
-yy <- cov2[[1]]
-browser()
+cov3 <- cov2[[1]]
 
 fit_lm_meta <- lm_meta(feature_abd = CRC_abd,
                        batch = opt$batch,
                        exposure = opt$exposure,
-                       covariates = yy,
+                       covariates = cov3,
                        data = CRC_meta,
                        control = list(verbose = FALSE))
 
 meta_fits <- fit_lm_meta$meta_fits
-   
-print("Creating graph")
-meta_fits %>% 
-  filter(qval.fdr < 0.05) %>% 
-  arrange(coef) %>% 
-  mutate(feature = factor(feature, levels = feature)) %>% 
-  ggplot(aes(y = coef, x = feature)) +
-  geom_bar(stat = "identity") +
-  coord_flip()
+write.csv(meta_fits, "meta_fits.csv")   
 
-print("Program ended")
