66930ef2073ba536bbd47d0c33e7c2b1cbefb72d
root <>
2022-02-21 01:06:01 +0000

    Updated lm_meta files

--- mmuphin/lm_meta_wrapper.sh
+++ mmuphin/lm_meta_wrapper.sh
@@ -3,9 +3,9 @@ CWD="$(pwd)"
 PGD="/galaxy-central/tools/mmuphin"
 feature_abd=$1
 batch=$2
-exposure=$3
-covariates=$4
-data=$5
+covariates=$3
+data=$4
+exposure=$5
 output1=$6
 
 Rscript ${PGD}/lm_meta_wrapper.R \
