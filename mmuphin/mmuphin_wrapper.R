6160a4e030389921d129cfac579669ccd9a01225
root <>
2022-02-21 20:21:49 +0000

    Update to mmuphin_wrapper.R to import csvs

--- mmuphin/mmuphin_wrapper.R
+++ mmuphin/mmuphin_wrapper.R
@@ -4,21 +4,22 @@ library(dplyr)
 library(ggplot2)
 library("optparse")
 option_list = list(
-    make_option(c("-f", "--feature_abd"), action="store", default="CRC_abd.Rda", type='character',
+    make_option(c("-f", "--feature_abd"), action="store", default="CRC_abd.csv", type='character',
               help="feature_abd"),
     make_option(c("-b", "--batch"), action="store", default="CRC_abd", type='character',
               help="Batch"),
     make_option(c("-c", "--covariates"), action="store", default="study_condition", type='character',
               help="covariates"),
-    make_option(c("-d", "--data"), action="store", default="CRC_meta.Rda", type='character',
+    make_option(c("-d", "--data"), action="store", default="CRC_meta.csv", type='character',
               help="Batch"),
     make_option(c("-o", "--output"), action="store", default="output1", type='character',
               help="output file")
     )
 
 opt = parse_args(OptionParser(option_list=option_list))
-load(opt$feature_abd)
-load(opt$data)
+
+CRC_abd  <- read.csv(file = opt$feature_abd)
+CRC_meta <- read.csv(file = opt$data)
 
 fit_adjust_batch <- adjust_batch(feature_abd = CRC_abd,
                                  batch = opt$batch,
