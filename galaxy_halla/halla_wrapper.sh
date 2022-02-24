4007162f3f688863f317355a0334c46dd1de94f0
root <>
2022-02-24 01:42:06 +0000

    Modified halla wrapper

--- galaxy_halla/halla_wrapper.sh
+++ galaxy_halla/halla_wrapper.sh
@@ -7,11 +7,12 @@ output2=$5
 
 Outlocation=$(mktemp -d)
 Outlocation2=$(mktemp -d)
+echo $Outlocation
+echo $Outlocation2
+halla -x $X -y $Y -m $m -o ${Outlocation}/halla_output
+cp $Outloaction/halla_output/hallagram.pdf $output1
 
-halla -x $X -y $Y -m $m -o $Outlocation
-cp ${Outlocation}/hallagram.pdf $output1
-
-zip -r  $Outlocation2/Halla_Results.zip  $Outlocation
-cp  $Outlocation2/Halla_Results.zip  $output2
+zip -r  $Outlocation2/Halla_Results.zip $Outlocation/halla_output
+cp  $Outlocation2/halla_output/Halla_Results.zip  $output2
 
 ###rm -r $Outlocation
