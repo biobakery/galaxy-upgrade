2e68190fdabbc1603f723e071864d3edf22e4aa2
root <>
2022-02-27 20:58:22 +0000

    Updated halla_wrapper for advanced features

--- galaxy_halla/halla_wrapper.sh
+++ galaxy_halla/halla_wrapper.sh
@@ -7,7 +7,22 @@ output2=$5
 fnr_thresh=$6
 fdr_alpha=$7
 
-halla -x $X -y $Y -m $m -o halla_output --fnr_thresh $fnr_thresh --fdr_alpha $fdr_alpha
+
+cmd="halla -x $X -y $Y -m $m -o halla_output"
+
+if  !  [ -z "$fnr_thresh" ]
+then
+     cmd="$cmd  --fnr_thresh $fnr_thresh"
+fi
+
+if !  [  -z "$fdr_alpha" ]
+then
+     cmd="$cmd  --fdr_alpha $fdr_alpha"
+fi
+
+eval $cmd
+
+
 cp halla_output/hallagram.pdf $output1
 
 zip -r  Halla_Results.zip halla_output
