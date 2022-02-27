7541d527c6d91233a86e525c9a8b41a116271759
George Weingart <george.weingart@gmail.com>
2022-02-27 11:25:09 -0800

    Updated halla.xml for avdvanced options

--- galaxy_halla/halla_wrapper.sh
+++ galaxy_halla/halla_wrapper.sh
@@ -4,8 +4,10 @@ Y=$2
 m=$3
 output1=$4
 output2=$5
+fnr_thresh=$6
+fdr_alpha=$7
 
-halla -x $X -y $Y -m $m -o halla_output
+halla -x $X -y $Y -m $m -o halla_output --fnr_thresh $fnr_thresh --fdr_alpha $fdr_alpha
 cp halla_output/hallagram.pdf $output1
 
 zip -r  Halla_Results.zip halla_output
