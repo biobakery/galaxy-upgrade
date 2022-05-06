199401a7c586bec96dd4d9d92856d81761fe4315
George Weingart <george.weingart@gmail.com>
2022-05-06 12:14:50 -0700

    Updated sparsedossa

--- sparssedossa/sparsedossa_wrapper.R
+++ sparssedossa/sparsedossa_wrapper.R
@@ -3,8 +3,6 @@ library('SparseDOSSA2')
 option_list = list(
     make_option(c("-t", "--template"), action="store", default="Stool", type='character',
               help="Template name"),
-    make_option(c("-d", "--odirname"), action="store", default="xxxx", type='character',
-              help="Output Directory Name"),
     make_option(c("-s", "--n_sample"), type = "integer", default = 100,
             help = "Number of samples [default %default]",
             metavar = "number"),
@@ -14,7 +12,6 @@ option_list = list(
 )
 
 opt = parse_args(OptionParser(option_list=option_list))
-OutputDir <- opt$odirname
 
 
 Simulation_Results_List  <- SparseDOSSA2(template = opt$template,
@@ -27,9 +24,9 @@ lNames <- names(Simulation_Results_List)
 for(i in 1:2) {
     df  <- as.data.frame(Simulation_Results_List[i])
     Output_FileName <- paste(lNames[i],"csv",sep=".")
-    Output_FileName <- paste(OutputDir,"/",Output_FileName,sep="")
+    Output_FileName <- paste(Output_FileName,sep="")
     write.csv(df ,Output_FileName)
                 }
-Simulation_Results_FileName <- paste(OutputDir,"/","Simulation.RData",sep="")
+Simulation_Results_FileName <- paste(Simulation.RData",sep="")
 
 save(Simulation_Results_List,file=Simulation_Results_FileName)
