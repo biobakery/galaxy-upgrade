4859fe218c4407cc79c002fbe548d4f9a8eca0ed
root <>
2022-05-09 15:29:20 +0000

    Modified sparsedossa wrapper

--- sparssedossa/sparsedossa_wrapper.R
+++ sparssedossa/sparsedossa_wrapper.R
@@ -8,7 +8,9 @@ p_nf <- as.numeric(myargs[3])
 p_metadata_effect_size <-  as.numeric(myargs[4])
 p_perc_feature_spiked_metadata <- as.double(myargs[5])
 p_median_read_depth <- as.numeric(myargs[6])
-p_spike_metadata <- myargs[7]
+p_spike_metadata0 <- myargs[7]
+p_spike_metadata1 <- gsub("dq","",p_spike_metadata0)
+p_spike_metadata <- gsub("_","", p_spike_metadata1)
 
 Simulation_Results_List<-SparseDOSSA2(
         template = p_template,
